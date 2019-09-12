356f3e73943470ac9dd78e4db01996e740315986
donghao.break <donghao.break@bytedance.com>
2019-09-12 15:05:16 +0800

    add threadpool

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,13 +2,18 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use hello::ThreadPool;
+
 fn main() {
   let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+  let pool = ThreadPool::new(4);
 
   for stream in listener.incoming() {
     let stream = stream.unwrap();
 
-    handle_connection(stream);
+pool.execute(|| {
+  handle_connection(stream);
+});
   }
 }
 
@@ -16,9 +21,16 @@ fn handle_connection(mut stream: TcpStream) {
   let mut buffer = [0; 512];
   stream.read(&mut buffer).unwrap();
 
-  let contents = fs::read_to_string("hello.html").unwrap();
+  let get = b"GET / HTTP/1.1\r\n";
 
-  let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+  let (status_line, filename) = if buffer.starts_with(get) {
+    ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+  } else {
+    ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+  };
+
+  let contents = fs::read_to_string(filename).unwrap();
+  let response = format!("{}{}", status_line, contents);
 
   stream.write(response.as_bytes()).unwrap();
   stream.flush().unwrap();
